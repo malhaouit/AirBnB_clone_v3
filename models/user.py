3921ab3f606347dc723eb0e28264ebae1d061d32
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-31 14:04:39 +0000

    Task 14: Security improvements!

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,18 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.password = self.__hash_password(self.password)
+
+    @staticmethod
+    def __hash_password(password):
+        """Hashes a password with MD5"""
+        if password:
+            return hashlib.md5(password.encode()).hexdigest()
+        return None
+
+    def to_dict(self, save_to_disk=False):
+        """Converts User object to dictionary representation"""
+        dict_repr = super().to_dict()
+        if not save_to_disk:
+            dict_repr.pop("password", None)
+        return dict_repr
